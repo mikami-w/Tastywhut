cb6c0f74360248c15d805a3876410937d275ac08
shenyi <11240771+Sy864086367@user.noreply.gitee.com>
2025-10-22 17:09:09 +0800

    added frontend files\

---
+++ frontend/miniprogram-1/pages/person/person.js
@@ -0,0 +1,14 @@
+// pages/person/person.js
+
+  Page({
+    goToMyEvaluation: function() {
+      wx.navigateTo({
+        url: '/pages/evaluate/evaluate'
+      });
+    }
+  });
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+ 
